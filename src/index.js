fe09d4e9c4b364892d2eb7f973ade40d88afee0e
ira <irin-ka@inbox.ru>
2018-09-25 15:20:43 +0300

    bracket

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig){
+  console.log(str);
+  var count = 0;
+  var found = 1;
+  while(found == 1){
+    found = 0;
+    for( var j=0; j < bracketsConfig.length;j++){
+      var arr = str.split(bracketsConfig[j][0]+bracketsConfig[j][1]);
+      if(arr.length > 1){
+        count++
+        found = 1;
+        str = arr.join('');
+      }
+    }
+  }
+  var result = false;
+  if(str == ''){
+    result = true;
+  }
+  console.log(result);
+  return result;
 }
